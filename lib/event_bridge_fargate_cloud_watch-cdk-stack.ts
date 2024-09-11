113c00bb117ded02c8628054f62a437f635bd090
jagathda <mail.jagath@gmail.com>
2024-09-11 19:50:41 +0300

    Added ECS cluster

--- lib/event_bridge_fargate_cloud_watch-cdk-stack.ts
+++ lib/event_bridge_fargate_cloud_watch-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 
 export class EventBridgeFargateCloudWatchCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,11 @@ export class EventBridgeFargateCloudWatchCdkStack extends cdk.Stack {
     // VPC for ECS
     const vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2
+    });
+
+    // ECS cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
     });
 
   }
